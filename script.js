1387cf1ed9ba2c47db29929d7ad972726cf6e76c
Leandro Henrique <110359031+xLarsMc@users.noreply.github.com>
2023-06-26 13:18:24 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const botao = document.querySelector(".botaoadiciona");
 const caixinha = document.querySelector(".dialog");
+const edit = document.querySelector(".edit");
 const sair = document.querySelector(".sair");
+const editSair = document.querySelector(".editSair");
 const adiciona = document.querySelector(".adc");
+const att = document.querySelector(".att");
 const inpTit = document.querySelector(".inpTit");
 const inpDesc = document.querySelector(".inpDesc");
 const inpImg = document.querySelector(".inpImg");
+const inpEditTit = document.querySelector(".inpEditTit");
+const inpEditDesc = document.querySelector(".inpEditDesc");
+const inpEditImg = document.querySelector(".inpEditImg");
 const postagens = document.querySelector(".postagens");
 var salvaAdc = []; 
 
@@ -17,6 +23,9 @@ botao.addEventListener("click",function(){
 sair.addEventListener("click", function(){
     caixinha.className = "dialog";
 });
+editSair.addEventListener("click", function(){
+    edit.className = "edit";
+});
 function adicionar(titulo, descricao, link){
     this.titulo = titulo;
     this.descricao = descricao;
@@ -24,7 +33,7 @@ function adicionar(titulo, descricao, link){
 }
 adiciona.addEventListener("click", function(){
     let adc = new adicionar(inpTit.value, inpDesc.value, inpImg.value);
-
+    
     salvaAdc.push(adc);
 
     localStorage.setItem("key", salvaAdc);
@@ -34,20 +43,59 @@ adiciona.addEventListener("click", function(){
     let desc = document.createElement("p");
     let img = document.createElement("img");
     let exc = document.createElement("button");
+    let pExc = document.createElement("p");
+    let editBt = document.createElement("button");
+    let pEdit = document.createElement("p");
 
     tit.innerHTML = adc.titulo;
     desc.innerHTML = adc.descricao;
     img.src = adc.link;
+    pEdit.textContent = "Editar";
+    pExc.textContent = "Excluir";
+    criaPost.classList.add("classDin");
 
+    exc.appendChild(pExc);
+    editBt.appendChild(pEdit);
     criaPost.appendChild(tit);
     criaPost.appendChild(desc);
     criaPost.appendChild(img);
     criaPost.appendChild(exc);
+    criaPost.appendChild(editBt);
     postagens.appendChild(criaPost);
+
+    exc.addEventListener("click", function(){
+        var excDiv = this.parentNode;
+        if (excDiv.classList.contains("classDin")){
+            excDiv.parentNode.removeChild(excDiv);
+        }
+    });
+    editBt.addEventListener("click", function(){
+
+        edit.className = "edit-mostrar";
+        
+        var divPai = this.parentNode;
+
+        var editTit = divPai.querySelector("h1");
+        var descTit = divPai.querySelector("p");
+        var imgTit = divPai.querySelector("img");
+
+        inpEditTit.value = editTit.innerHTML;
+        inpEditDesc.value = descTit.innerHTML;
+        inpEditImg.value = imgTit.src;
+
+        att.addEventListener("click", function(){
+            var novoTit = inpEditTit.value;
+            var novoDesc = inpEditDesc.value;
+            var novaImg = inpEditImg.value;
+
+            tit.innerHTML = novoTit;
+            desc.innerHTML = novoDesc;
+            imgTit.src = novaImg;
+
+        });
+    })
+    
 });
 
-exc.addEventListener("click", function(){
-    document.body.removeChild(document.body.querySelector("postagens"));
-});
 
 
